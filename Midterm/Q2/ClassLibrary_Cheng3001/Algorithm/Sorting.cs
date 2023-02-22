168dd7b8292105f8320cee69c8de2e2cfab4431a
ye1206 <116014300+ye1206@users.noreply.github.com>
2023-02-22 22:13:32 +0800

    Add files via upload

---
+++ Midterm/Q2/ClassLibrary_Cheng3001/Algorithm/Sorting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary_Cheng3001.Algorithm
+{
+    public class Sorting
+    {
+        public static int i = 0; // for loop
+        public static int j = 0; //for loop
+        public static int k = 0;
+        public static int temp = 0;
+        public static int start = 0;
+
+        public static void BubblieSort(int[] data)
+        {
+            int max, min;
+
+            // max = -int32.maxvalue; 
+            // min = int32.maxvalue;
+
+            // the other way
+            max = data[0];
+            min = data[0];
+
+            for (i = 0; i < data.Length; i++)
+            {
+                if (data[i] > max)
+                    max = data[i];
+                if (data[i] < min)
+                    min = data[i];
+            } //for
+
+
+
+        } //BubbleSort
+
+        public static void Cocktail(int[] data)
+        {
+            int end = data.Length;
+
+            for (i = start; i < end - 1; i++) //sort
+            {
+                for (j = start; j < end - 1; j++) // 由小到大排序 
+                {
+                    while (data[j] > data[j + 1])
+                    {
+                        temp = data[j];
+                        data[j] = data[j + 1];
+                        data[j + 1] = temp;
+                    }
+                }
+
+                end = end - 1;
+
+                for (j = end; j > start; j--) // 從數列尾由大到小排序
+                {
+                    while (data[j] < data[j - 1])
+                    {
+                        temp = data[j];
+                        data[j] = data[j - 1];
+                        data[j - 1] = temp;
+                    }
+                }
+
+                start = start + 1;
+
+                Console.Write("第 {0} 回合排序\t", i + 1);
+                foreach (var item in data)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            } // end of outer loop
+        }
+    }
+}
---
+++ Midterm/Q2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        public class Stack
+        {
+            public int MaxSize;
+            public string[] MyStack;
+            public int top;
+
+            public Stack() //default
+            {
+                MaxSize = 0;
+                top = 0;
+                MyStack = null;
+            } //constructor
+
+            public Stack(int val) //overload
+            {
+                MaxSize = val;
+                MyStack = new string[MaxSize];
+                top = 0;
+            } //constructor
+
+            #region Empty or Full
+            public bool isEmpty()
+            {
+                if (top == 0)
+                    return true;
+                else
+                    return false;
+            } //isEmpty
+
+            public bool isFull()
+            {
+                if (top == MaxSize)
+                    return true;
+                else
+                    return false;
+            } //isFull
+            #endregion Empty or Full
+
+            #region Push and Pop
+            public string Pop()
+            {
+                if (isEmpty())
+                {
+                    Console.WriteLine("The stack is empty");
+                    return "";
+                } //if
+                else
+                {
+                    top--;
+                    Console.WriteLine($"\n{MyStack[top]} is poped, top = {top}");
+                    return MyStack[top];
+                } //else
+            } //Pop
+
+            public void Push(string obj)
+            {
+                if (isFull())
+                    Console.WriteLine("Stack is full, cannot be pushed...");
+                else
+                {
+                    MyStack[top] = obj;
+                    top++;
+                    Console.WriteLine($"{obj} is added into the stack, top = {top}");
+                } //else
+            } //Push
+            #endregion Push and Pop
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($"\nPrint Current Stack, top = {top}");
+
+                if (!isEmpty())
+                {
+                    int i = 0;
+                    for (i = top - 1; i >= 0; i--)
+                        Console.WriteLine(MyStack[i]);
+                    Console.WriteLine();
+                } //if
+                else
+                    Console.WriteLine("Stack has nothing to show...");
+            } //ShowInfo
+        } //Stack
+        static void Main(string[] args)
+        {
+            #region name
+            Console.WriteLine("110213076 鄭鈺曄");
+            #endregion name
+
+            Stack mystack = new Stack(8);
+            Console.Write("Please input the instruction: ");
+            string x = Console.ReadLine();
+            string[] command = x.Split(' ');
+            int i = 0;
+
+            for (i = 0; i < command.Length; i++)
+            {
+                if (command[i] == "1")
+                    mystack.Push(command[i + 1]);
+                else if (command[i] == "2")
+                    mystack.Pop();
+                else if (command[i] == "3")
+                    mystack.ShowInfo();
+            } //for
+            Console.ReadLine();
+        }
+    }
+}
