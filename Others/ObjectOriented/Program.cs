ed838788d71bb35cd705effbf11f9a8a1f9e61c1
ye1206 <116014300+ye1206@users.noreply.github.com>
2023-02-22 21:55:03 +0800

    Add files via upload

---
+++ Others/ObjectOriented/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ObjectOriented
+{
+    internal class Program
+    {
+        public class demo
+        {
+            public int myValue;
+            public string myName;
+            public demo() //constructor
+            {
+                myValue = 0;
+                myName = "";
+            }
+            public demo(int v, string n) //constructor
+            {
+                myValue = v;
+                myName = n;
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine("value = {0}, name = {1}", myValue, myName);
+            } //ShowInfo
+
+        } //demo
+
+        static void Main(string[] args)
+        {
+            Random myRandom = new Random();
+            int val = myRandom.Next(51);
+
+            demo obj1 = new demo();
+            obj1.myValue = 10;
+            obj1.myName = "John Lee";
+            obj1.ShowInfo();
+
+            Console.Write("------------------------------------------------------------\n");
+
+            demo obj2 = new demo(20, "IM");
+            obj2.ShowInfo();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ Others/polynomial/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using ClassLibrary_Cheng3001.DSA;
+
+namespace polynomial
+{
+    class Program
+    {
+        struct Poly
+        {
+            public int coeff; //係數
+            public int exop;  //次方
+        }
+
+        static void Main(string[] args)
+        {
+            #region global
+            string[] poly_a, poly_b;
+            int i = 0;
+            int indexA, indexB, totalTerms;
+            #endregion global
+
+            #region Polynomial
+            Console.Write("輸入多項式A = ");
+            poly_a = Console.ReadLine().Split(); //對多項式作切割並儲存
+            //Poly[] polyA = new Poly[poly_a.Length / 2];
+
+            //for (i = 0; i < polyA.Length; i++)
+            //{
+            //    polyA[i].exop = int.Parse(poly_a[i * 2]); // 跳格儲存 (切割後儲存方式為 係數 次方 係數 次方...)
+            //    polyA[i].coeff = int.Parse(poly_a[i * 2 + 1]);
+            //}
+
+            Console.Write("輸入多項式B = ");
+            poly_b = Console.ReadLine().Split(); //對多項式作切割並儲存
+            //Poly[] polyB = new Poly[poly_b.Length / 2];
+
+            //for (i = 0; i < polyB.Length; i++)
+            //{
+            //    polyB[i].exop = int.Parse(poly_b[2 * i]); // 跳格儲存 (切割後儲存方式為 係數 次方 係數 次方...)
+            //    polyB[i].coeff = int.Parse(poly_b[2 * i + 1]);
+            //}
+
+            //Poly[] polyC = new Poly[polyA.Length + poly_b.Length];
+            //indexA = 0;
+            //indexB = 0;
+            //totalTerms = 0;
+
+            //while ((indexA < polyA.Length) && (indexB < polyB.Length))
+            //{
+            //    if (polyA[indexA].exop > polyB[indexB].exop)
+            //    {
+            //        polyC[totalTerms].exop = polyA[indexA].exop;
+            //        polyC[totalTerms].coeff = polyA[indexA].coeff;
+            //        totalTerms++;
+            //        indexA++;
+            //    } //if
+            //    else if (polyA[indexA].exop == polyB[indexB].exop)
+            //    {
+            //        polyC[totalTerms].exop = polyA[indexA].exop;
+            //        polyC[totalTerms].coeff = polyA[indexA].coeff + polyB[indexB].coeff;
+            //        totalTerms++;
+            //        indexA++;
+            //        indexB++;
+            //    } //else if 
+            //    else
+            //    {
+            //        polyC[totalTerms].exop = polyB[indexB].exop;
+            //        polyC[totalTerms].coeff = polyB[indexB].coeff;
+            //        totalTerms++;
+            //        indexB++;
+            //    } // else
+
+            //    while (indexA < polyA.Length)
+            //     {
+            //        polyC[totalTerms] = polyA[indexA];
+            //        //poly_c[totalTerms].coeff = poly_a[indexA].coeff;
+            //        totalTerms++;
+            //        indexA++;
+            //     } //while indexA < polyA.Length
+            //    while (indexB < polyA.Length)
+            //     {
+            //        polyC[totalTerms] = polyB[indexB];
+            //        //poly_c[totalTerms].coeff = poly_a[indexA].coeff;
+            //        totalTerms++;
+            //        indexB++;
+            //     } //while indexB < polyB.Length
+
+            //} //while
+
+            #endregion polynomial
+
+            //for (i = 0; i < totalTerms; i++)
+            //    Console.Write($"{polyC[i].coeff} x^{polyC[i].exop} ");
+
+            Polynomial.polynomial(poly_a, poly_b);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
