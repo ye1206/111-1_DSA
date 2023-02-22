a25bb6a05770df23eddb7b55321e1ae7a6c567ca
ye1206 <116014300+ye1206@users.noreply.github.com>
2023-02-22 22:28:25 +0800

    Add files via upload

---
+++ Recursion/Q7/Program.cs
@@ -0,0 +1,66 @@
+﻿//F(0) = 0, F(1) = 1
+//F(2n) = 2 * F(n) - 1, n > 1
+//F(2n + 1) = 2* F(n) + 1, n > 0
+using System;
+
+namespace Q7
+{
+    public class Program
+    {
+        public class Recursive
+        {
+            private int _n;
+            private int _result;
+            public Recursive()
+            {
+                _n = 0;
+                _result = 0;
+            } //constructor
+
+            public Recursive(int a)
+            {
+                _n = a;
+                _result = 0;
+            } //constructor
+
+            public int RecurFunc(int n)
+            {
+                if (n == 0)
+                    return 0;
+                else if (n == 1)
+                    return 1;
+                else
+                {
+                    if (n % 2 == 0)
+                        return (3 * RecurFunc(n) - 1);
+                    else if (n % 3 == 1)
+                        return (3 * RecurFunc(n));
+                    else
+                        return (3 * RecurFunc(n) + 1);
+                } //else
+            } //RecurFunc
+
+            public void ShowResult()
+            {
+                _result = RecurFunc(_n);
+                Console.WriteLine($"n = {_n}, ans = {_result}\n");
+            } //ShowResult
+        } //class Recursive
+        static void Main(string[] args)
+        {
+            Recursive r = new Recursive(51);
+            r.ShowResult();
+            Console.ReadLine();
+            //int i = 0;
+            //Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            //for (i = 0; i < 5; i++)
+            //{
+            //    Recursive obj = new Recursive(rnd.Next(1, 21));
+            //    obj.ShowResult();
+            //} //for
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Recursion/Q8/Program.cs
@@ -0,0 +1,61 @@
+﻿// F(m, n) = n + 1, m = 0
+// F(m, n) = F(m - 1, 1), n =0
+// F(m, n) = F(m - 1, F(m, n - 1)), otherwise
+using System;
+
+namespace Q8
+{
+    class Program
+    {
+        public class Recursive
+        {
+            int _m;
+            int _n;
+            int _result;
+
+            public Recursive()
+            {
+                _m = _n = _result = 0;
+            } //constructor
+
+            public Recursive(int m, int n)
+            {
+                _m = m;
+                _n = n;
+                _result = 0;
+            } //constructor
+
+            public int RecurFunc(int m, int n)
+            {
+                if (m == 0)
+                    return n + 1;
+                else
+                {
+                    if (n == 0)
+                        return RecurFunc(m - 1, 1);
+                    else
+                        return RecurFunc(m - 1, RecurFunc(m, n - 1));
+                } //else
+            } //RecurFunc
+
+            public void ShowResult()
+            {
+                _result = RecurFunc(_m, _n);
+                Console.WriteLine("F({0}, {1}) = {2}", _m, _n, _result);
+            } //ShowResult
+        } //class Recursive
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            int i = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+                Recursive obj = new Recursive((rnd.Next(1, 4)), (rnd.Next(4, 10)));
+                obj.ShowResult();
+            } //for
+
+            Console.ReadLine();
+        }
+    }
+}
