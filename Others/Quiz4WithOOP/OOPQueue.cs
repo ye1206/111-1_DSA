3d769925c8c66486b8a9747145a5dc16313982d2
ye1206 <116014300+ye1206@users.noreply.github.com>
2023-02-22 21:56:40 +0800

    Add files via upload

---
+++ Others/Quiz4WithOOP/OOPQueue.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz4WithOOP
+{
+    public class OOPQueue
+    {
+        #region global
+        public int MaxSize;
+        public int Front;
+        public int Rear;
+        public int HowMany;
+        public int[] CircularQueue;
+        public int[] pickup;
+        #endregion global
+
+        public OOPQueue(int maxsize)
+        {
+            this.MaxSize = maxsize + 1;
+            HowMany = 0;
+            CircularQueue = new int[MaxSize];
+            pickup = new int[maxsize];
+        } //OOPQueue (constructor)
+
+        #region Full or Empty
+        public bool isEmpty()
+        {
+            if (Front == Rear)
+                return true;
+            else
+                return false;
+        } //isEmpty
+
+        public bool isFull()
+        {
+            if (((Rear + 1) % MaxSize) == Front)
+                return true;
+            else
+                return false;
+        } //isFull
+        #endregion Full or Empty
+
+        #region Enqueue and Dequeue
+        public void Enqueue(int num)
+        {
+            if (!isFull())
+            {
+                CircularQueue[Rear] = num;
+                Rear = (Rear + 1) % MaxSize;
+            } //if
+            // Console.WriteLine("Queue is full, {0} cannot be enqueued.", num);
+        } //Enqueue
+
+        public void Dequeue(ref int val)
+        {
+            if (!isEmpty())
+            {
+                val = CircularQueue[Front];
+                // Console.WriteLine("{0} is removed from queue", CircularQueue[Front]);
+                Front = (Front + 1) % MaxSize;
+            } //if
+        } //Dequeue
+        #endregion Enqueue and Dequeue
+
+        public void Process()
+        {
+            #region global
+            int i = 0;
+            int value = 0;
+            #endregion global
+
+            for (i = 1; i <= MaxSize; i++)
+                Enqueue(i);
+
+            while (!isEmpty())
+            {
+                Dequeue(ref value);
+                pickup[HowMany] = value;
+                HowMany++;
+
+                if (isEmpty())
+                    break;
+                else
+                {
+                    Dequeue(ref value);
+                    Enqueue(value);
+                } //else
+            } //while
+        } //Process
+
+        public void ShowAnswer()
+        {
+            int i = 0;
+            for (i = 0; i < HowMany; i++)
+                Console.Write("{0}", pickup[i].ToString().PadLeft(3));
+            Console.WriteLine();
+        } //ShowAnswer
+    } //OOPQueue
+} //Quiz$WithOOP
+    
---
+++ Others/Quiz4WithOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Quiz4WithOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.Write("請輸入5個整數: ");
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            for (i = 0; i < numbers.Length; i++)
+            {
+                OOPQueue obj1 = new OOPQueue(numbers[i]);
+                obj1.Process();
+                obj1.ShowAnswer();
+                Console.WriteLine();
+            } //for
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        } //Main
+    }
+}
