85a48b3b8610e5c1ef2ed149e3c3ebc7e8c20a97
ye1206 <116014300+ye1206@users.noreply.github.com>
2023-02-22 22:38:26 +0800

    Add files via upload

---
+++ Final/Q2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("110213076 鄭鈺曄");
+
+            int[] data = new int[10];
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            int i = 0;
+            int min = 0;
+
+            for (i = 0; i < 10; i++)
+                data[i] = rnd.Next(-100,100);
+
+            Console.Write("原始資料: ");
+            foreach (int obj in data)
+                Console.Write(obj + " ");
+            Console.WriteLine();
+
+            for (i = 0; i < 10; i++)
+            {
+                min = Math.Min(min, data[i]);
+            } //for
+
+            Console.WriteLine($"最小值 ={min}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Final/Q3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Q3
+{
+    class Program
+    {
+        public class Recursive
+        {
+            public int _n;
+            private int _result;
+            public Recursive()
+            {
+                _n = 0;
+                _result = 0;
+            } //constructor
+
+            public Recursive(int a)
+            {
+                _n = a;
+                _result = 0;
+            } //constructor
+
+            public int RecurFunc(int n)
+            {
+                if (n == 0)
+                    return 0;
+                else if (n == 1)
+                    return 1;
+                else if (n == 2)
+                    return 2;
+                else
+                {
+                    if (n % 3 == 0)
+                        return (3 * RecurFunc(n) - 1);
+                    else if (n % 3 == 1)
+                        return (3 * RecurFunc(n));
+                    else
+                        return (3 * RecurFunc(n) + 1);
+                } //else
+            } //RecurFunc
+
+            public void ShowResult()
+            {
+                _result = RecurFunc(_n);
+                Console.WriteLine($"n = {_n}, ans = {_result}\n");
+            } //ShowResult
+        } //class Recursive
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("110213076 鄭鈺曄");
+
+            Recursive obj = new Recursive(51);
+            obj.ShowResult();
+
+            Console.ReadLine();
+        }
+    }
+}
