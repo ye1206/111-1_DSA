bd9aebc93d361b4b7769fb050329252602c5e9eb
ye1206 <116014300+ye1206@users.noreply.github.com>
2023-02-22 22:23:56 +0800

    Add files via upload

---
+++ Recursion/Q2_Fibonacci/Program.cs
@@ -0,0 +1,61 @@
+﻿//費氏數列
+using System;
+
+namespace Q2_Fibonacci
+{
+    class Program
+    {
+        public class Fibon_Recursive01
+        {
+            int _n;
+            int _result;
+
+            public Fibon_Recursive01()
+            {
+                _n = 1;
+                _result = 1;
+            } //constructor Fibon_Recursive01
+
+            public Fibon_Recursive01(int a)
+            {
+                if (a > 0)
+                    _n = a;
+                else
+                    _n = 1;
+
+                _result = 1;
+            } //constructor Fibon_Recursive01
+
+            public int Func01(int n)
+            {
+                if (n == 1)
+                    return 1;
+                else if (n == 2)
+                    return 1;
+                else
+                    return Func01(n - 1) + Func01(n - 2);
+            } //Func01
+
+            public void ShowInfo()
+            {
+                _result = Func01(_n);
+                Console.WriteLine($"F[{_n}] = {_result}\n");
+            } //ShowInfo
+        } //Fibon_Recursive01
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int seed = (int)DateTime.Now.Ticks;
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            //Random rnd = new Random(seed);
+
+            for (i = 0; i < 5; i++)
+            {
+                Fibon_Recursive01 obj = new Fibon_Recursive01(rnd.Next(1, 15));
+                obj.ShowInfo();
+            } //for
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Recursion/Q3_Fibonacci/Program.cs
@@ -0,0 +1,57 @@
+﻿// sum(n) = n + (n - 1) + ... + 1
+using System;
+
+namespace Q3_Fibonacci
+{
+    class Program
+    {
+        public class Fibon_Recursive02
+        {
+            int _n;
+            int _result;
+
+            public Fibon_Recursive02()
+            {
+                _n = 1;
+                _result = 1;
+            } //constructor Fibon_Recursive01
+
+            public Fibon_Recursive02(int a)
+            {
+                if (a > 0)
+                    _n = a;
+                else
+                    _n = 1;
+
+                _result = 1;
+            } //constructor Fibon_Recursive01
+
+            public int Func02(int n)
+            {
+                if (n == 1)
+                    return 1;
+                else
+                    return n + Func02(n - 1);
+            } //Func01
+
+            public void ShowInfo()
+            {
+                _result = Func02(_n);
+                Console.WriteLine($"F[{_n}] = {_result}\n");
+            } //ShowInfo
+        } //Fibon_Recursive02
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            for (i = 0; i < 5; i++)
+            {
+                Fibon_Recursive02 obj = new Fibon_Recursive02(rnd.Next(1, 15));
+                obj.ShowInfo();
+            } //for
+            
+        }
+    }
+}
