b69b2e1793743e7cee73bb626a62ec84af0e0364
ye1206 <116014300+ye1206@users.noreply.github.com>
2023-02-22 21:43:29 +0800

    Add files via upload

---
+++ Sorting/221019_InsertionSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ClassLibrary_Cheng3001.Algorithm;
+
+namespace _221019_InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region global
+            int[] data = new int[] { 3, 4, 1, 7, 8};
+            Console.Write("排序前的資料".PadLeft(4));
+            foreach (int var in data)
+                Console.Write(var.ToString().PadLeft(4));
+            Console.WriteLine();
+
+            int temp = 0;
+            int i, j;
+            #endregion global
+
+            Sorting.InsertionSort(data);
+
+            //#region Insertion Sort
+            //for (i = 1; i < data.Length; i++)
+            //{
+            //    temp = data[i];
+            //    for (j = i - 1; j >= 0; j--)
+            //    {
+            //        if (data[j] > temp)
+            //        {
+            //            data[j + 1] = data[j];
+            //            if (j == 0)
+            //                data[j] = temp;
+            //        }
+            //        else
+            //        {
+            //            data[j + 1] = temp;
+            //            break;
+            //        }
+            //    } // inner loop
+            //    Console.Write("第{0}回合排序: ", i);
+            //    foreach (int var in data)
+            //        Console.Write(var.ToString().PadLeft(4));
+            //    Console.WriteLine();
+            //} //for
+            //#endregion Insertion Sort
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sorting/221019_SelectionSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ClassLibrary_Cheng3001.Algorithm;
+
+namespace _221019_SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region global
+            int[] data = new int[] { 123, 23, 98, 41, -2, 69 };
+            int i = 0, j = 0;
+            int min_position = 0, temp = 0;
+
+            #endregion global
+
+            Console.Write("原始資料 ");
+            foreach (int var in data)
+                Console.Write(var.ToString().PadLeft(4));
+            Console.WriteLine("\n");
+
+            Sorting.SelectionSort(data);
+
+            #region Selection Sort
+            //for (i = 0; i < data.Length - 1; i++)
+            //{
+            //    min_position = i;
+            //    for (j = i + 1; j < data.Length; j++)
+            //    {
+            //        if (data[j] < data[min_position])  //如果發現[j]的數字比[min_position]的數字小，就將j取代min_position 
+            //            min_position = j;  //remember the position
+            //    } // inner loop
+
+            //    temp = data[i];
+            //    data[i] = data[min_position];
+            //    data[min_position] = temp;   // 25-27行即完成一回合的交換
+
+            //    Console.Write($"第{i + 1}回合排序結果\t");
+
+            //    for (j = 0; j < data.Length; j++)
+            //        Console.Write($"{data[j].ToString().PadLeft(4)}");
+            //    Console.WriteLine("\n");
+            //} //outer loop
+            #endregion Selection Sort
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
